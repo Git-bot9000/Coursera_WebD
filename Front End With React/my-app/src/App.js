1b898a9393281099db7a7a5a2a8479f8650902da
Git-bot9000 <amandeep09020@gmail.com>
2020-07-23 15:02:24 +0530

    Second Commit

--- Front End With React/my-app/src/App.js
+++ Front End With React/my-app/src/App.js
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import './App.css';
-import Message from './components/Message';
+import Menu from './components/MenuComponent';
+import {DISHES} from './shared/dishes';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/"> Namaste </NavbarBrand>
-        </div>
-      </Navbar>
-      <Message />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/"> Namaste </NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes}/>
+      </div>
+    );
+  }
 }
 
 export default App;
